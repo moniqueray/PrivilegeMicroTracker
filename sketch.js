1984385f6df5ea90e40d0b4b1a163e38b04928cc
moniqueray <80477992+moniqueray@users.noreply.github.com>
2022-05-25 09:03:58 -0700

    update

--- sketch.js
+++ sketch.js
@@ -9,74 +9,28 @@
 
     ***********************************************************************************/
 
-  // Global Variable
     var complexStateMachine;           // the ComplexStateMachine class
     var clickablesManager;             // our clickables manager
     var clickables;                    // an array of clickable objects
     
     var currentStateName = "";
-    var moodImage; //POSSIBLY CHANGEEEE
-
-    // Data Players: Republican Wilson
-    var reason;
-    var icons = [];
-    var scores = [0, 0, 0 ,0 , 10];
-    var solved = [false, false, false, false, true];
+    var backgroundImage;
     
-
-    // Colors
     var bkColor = '#031927';
     var textColor = '#E9D6EC';
     
-    // Splash Page
-    //INSERT
-
-    // Players 
-    var republican;
-    var commissioner;
-    var advocate;
-    var tech;
-    var reformative = [];
-
-
-
-    // var scores = [];
-    // const Justin = 0;
-    // const Celia = 1 ;
-    // const Ashley = 2;
-    // const Herbert = 3;
+    var buttonFont;
     
-    // var displayPecentages = true;
-    // var displayHorizontalBars = true;
-    
-    // var xLeftMargin = 50;     // for percentages
-    // var xBarOffset = 30;
-    // var yTopMargin = 50;
-    // var yOffset = 50;
-    // var barWidth = 30;
-    // var barSpacing = 50;
-    
-    // var buttonFont;
-    
-    // var lastButtonPressed = "";
-    
-    // Functions p5.js
     function preload() {
       clickablesManager = new ClickableManager('data/clickableLayout.csv');
       complexStateMachine = new ComplexStateMachine("data/interactionTable.csv", "data/clickableLayout.csv");
-      
-
-      // Preload fonts
-      buttonFont = loadFont("smoochsans.ttf");
-
-      // Preload splasj images
-      splashPic[0] = loadImage('assets/')
-      
+    
+      buttonFont = loadFont("AtariClassic-ExtraSmooth.ttf");
     }
     
     // Setup code goes here
     function setup() {
-      createCanvas(1280, 720);
+      createCanvas(1920, 1080);
       imageMode(CENTER);
     
       // setup the clickables = this will allocate the array
@@ -88,12 +42,9 @@
       // call OUR function to setup additional information about the p5.clickables
       // that are not in the array 
       setupClickables(); 
-    
-      console.log("scores = ");
-      console.log(scores);
-      initializeScores();
      }
-     
+    
+    
     // Draw code goes here
     function draw() {
       drawBackground();
@@ -108,14 +59,14 @@
         clickables[i].onHover = clickableButtonHover;
         clickables[i].onOutside = clickableButtonOnOutside;
         clickables[i].onPress = clickableButtonPressed;
-        clickables[i].textFont = "smoochsans";
-        clickables[i].width = 170;
+        clickables[i].textFont = "AtariClassic-ExtraSmooth";
+        clickables[i].width = 220;
       }
     }
     
     // tint when mouse is over
     clickableButtonHover = function () {
-      this.color = "#9E98A3";
+      this.color = "#F2545B";
       this.noTint = false;
       this.tint = "#FF0000";
     }
@@ -123,60 +74,30 @@
     // color a light gray if off
     clickableButtonOnOutside = function () {
       // backto our gray color
-      this.color = "#F8FADE";
+      this.color = "#E9D6EC";
     }
     
     clickableButtonPressed = function() {
       complexStateMachine.clickablePressed(this.name);
-    
-      // store the last button name pressed
-      lastButtonPressed = this.name;
     }
     
     // this is a callback, which we use to set our display image
     function setImage(imageFilename) {
-      moodImage = loadImage(imageFilename);
+      backgroundImage = loadImage(imageFilename);
     } 
     
     // this is a callback, which we can use for different effects
     function stateChanged(newStateName) {
         currentStateName = newStateName;
         console.log(currentStateName);
+    } 
     
-        // Change scores here
-        if( newStateName === "A1justin" ) {
-          // Good for Justin, bad for Herbert
-          scores[Justin] += 30;
-          scores[Celia] -= 10;
-          scores[Ashley] += 20;
-          scores[Herbert] -= 5;
-        }
-        if( newStateName === "A2justin" ) {
-          scores[Justin] -= 15;
-          scores[Celia] += 20;
-          scores[Ashley] -= 5;
-          scores[Herbert] += 10;
-        }
-      if( newStateName === "A3justin" ) {
-        scores[Justin] += 30;
-        scores[Celia] -= 5;
-        scores[Ashley] += 40;
-        scores[Herbert] += 5;
-      }
-      if( newStateName === "A4justin" ) {
-        scores[Justin] -= 10;
-        scores[Celia] += 5;
-        scores[Ashley] += 20;
-        scores[Herbert] -= 5;
-      }
-      if( newStateName === "A5justin" ) {
-        scores[Justin] += 10;
-        scores[Celia] += 10;
-        scores[Ashley] += 5;
-        scores[Herbert] += 5;
-      }
-       
-        console.log(scores);
+    
+    //==== KEYPRESSED ====/
+    function mousePressed() {
+      // if( currentStateName === "Splash" ) {
+      //   complexStateMachine.newState("Instructions");
+      // }
     }
     
     //==== MODIFY THIS CODE FOR UI =====/
@@ -185,37 +106,28 @@
       background(color(bkColor));
     }
     
+    // draw centered
     function drawImage() {
-      if( moodImage !== undefined ) {
-        image(moodImage, width/2, height/2);
+      if( backgroundImage !== undefined ) {
+        image(backgroundImage, width/2, height/2);
       }  
     }
     
     function drawOther() {
       push();
     
-       
+       // Draw mood — if not on Splash or Instructions screen  
+       if( currentStateName !== "Splash" && currentStateName !== "Instructions") {
+        fill(color(textColor));
+        textFont(buttonFont);
+        textSize(24);
+        text(currentStateName, width/2, 50);
       }
     
-      //if( currentStateName !== "Instructions" && currentStateName !== "Players") {
-       // fill(color(textColor));
-        //rect(200,300,100,20);
-        //textFont(buttonFont);
-        //textSize(36);
-        //text(currentStateName, width/2, 50);
-    
-      //}
-    
       pop();
-    
+    }
     
     //-- right now, it is just the clickables
     function drawUI() {
       clickablesManager.draw();
-    }
-    
-    
-    function initializeScores() {
-      scores = [50,50,50,50];
-      console.log(scores);
     }
